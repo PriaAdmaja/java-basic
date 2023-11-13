8ef55993d52b43b6ef5841279f40a0996c0890a6
PriaAdmaja <priaadmaja@gmail.com>
2023-11-14 06:18:38 +0700

    edit value

--- src/SortValue.java
+++ src/SortValue.java
@@ -2,10 +2,10 @@ import java.util.Arrays;
 
 public class SortValue {
     public static void main(String[] args) {
-        int[] rawData = { 4, 3, 6, 4, 8 };
+        int[] rawData = {3, 2, 5, 1, 4};
         for (int i = 0; i <= rawData.length - 2; i++) {
-            int temp;
             for (int j = i + 1; j <= rawData.length - 1; j++) {
+                int temp;
                 if (rawData[i] > rawData[j]) {
                     temp = rawData[i];
                     rawData[i] = rawData[j];
