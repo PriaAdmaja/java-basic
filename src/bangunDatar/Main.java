d97ea0205411e231d3e12ad065e7d770cd2d640d
PriaAdmaja <priaadmaja@gmail.com>
2023-11-15 05:37:38 +0700

    main file

---
+++ src/bangunDatar/Main.java
@@ -0,0 +1,15 @@
+package bangunDatar;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            Scanner scanner = new Scanner(System.in);
+            
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.out.println(e.getMessage());
+        }
+    }
+}
